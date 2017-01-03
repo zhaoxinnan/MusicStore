3b6900f96e1e78a50df155e7c622499e1031864e
zhao <zxn2012@qq.com>
2017-01-03 23:23:46 +0800

    page30

---
+++ MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse,Genre=" + genre);
+            return message;
+        }
+        public string Details()
+        {
+            return "hello from store.details()";
+        }
+    }
+}
