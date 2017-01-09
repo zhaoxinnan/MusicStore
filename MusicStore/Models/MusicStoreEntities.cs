e5f4161a98db68c5c07870eccc7d0ac5270a12f4
zhao <zxn2012@qq.com>
2017-01-09 10:55:08 +0800

    做到了生成视图，没成功

--- MusicStore/Models/MusicStoreEntities.cs
+++ MusicStore/Models/MusicStoreEntities.cs
@@ -11,5 +11,11 @@ namespace MusicStore.Models
     {
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        //public System.Data.Entity.DbSet<MusicStore.Models.Artist> Artists { get; set; }
     }
 }
